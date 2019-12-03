043dc64fc700e3ee62d0c533802a835b4f87ba12
Shobhit Maheshwari <shobhitm.iit@gmail.com>
2019-12-02 23:09:04 -0800

    : not working

--- hello.py
+++ hello.py
@@ -1,18 +1,23 @@
 from flask import Flask, render_template, Response
 from flask_socketio import SocketIO, join_room, emit
 import cv2
+import base64
+import numpy as np
 
 app = Flask(__name__, template_folder='.')
 socketio = SocketIO(app)
 
+def readb64(encoded_data):
+    nparr = np.fromstring(base64.b64decode(encoded_data), np.uint8)
+    return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    #sbuf = StringIO()
+    #sbuf.write(base64.b64decode(base64_string))
+    #pimg = Image.open(sbuf)
+    #return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+
 @app.route('/')
 def index():
     return render_template('index.html')
-
-#not used currently
-@app.route('/get')
-def get(x):
-    print(x)
 
 #not used currently
 def gen(camera):
@@ -25,7 +30,15 @@ def gen(camera):
 def on_create(data):
     print(data)
     #data = cv2.cvtColor(data, cv2.COLOR_BGR2GRAY) # need to first decode from base64
-    emit('join_room', data) # emit data back as it is
+    
+    x = len('data:image/png;base64,')
+    image = readb64(data[x:])
+    #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    g = np.array(cv2.imencode('.jpg', image)[1]).tostring()
+    print(g)
+    r = x+base64.b64encode(g)
+
+    emit('join_room', r) # emit data back as it is
 
 
 if __name__ == '__main__':
